e9187d75ae3e415906aa2fe7b1c32963f1a1b3df
Garrett Robinson <garrett.f.robinson@gmail.com>
2014-09-10 12:26:40 -0700

    Autobuild pdf

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ set -e
 mkdir build
 
 # compile encryption_works.md with pandoc
-pandoc -s -t html5 encryption_works.md > build/encryption_works.html
+pandoc -s -t html5 encryption_works.md -o build/encryption_works.html
+pandoc -s -t latex encryption_works.md -o build/encryption_works.pdf
